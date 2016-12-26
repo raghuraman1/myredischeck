df7fef29f29af470f34aa5f24e2922a581c870b9
raghuraman1 <raghuraman1@yahoo.com>
2016-12-26 07:22:50 +0530

    compile

--- src/main/java/myjdbceg/Application.java
+++ src/main/java/myjdbceg/Application.java
@@ -6,12 +6,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude={RedisAutoConfiguration.class})
+@EnableAutoConfiguration(exclude={RedisAutoConfiguration.class, RedisRepositoriesAutoConfiguration.class})
 public class Application {
 	
 	@Bean
